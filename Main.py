d038c5e87f453a8762ab87ec412b8b3fb3ca8cf6
Pontus Erlingsson <erpo16@nti.umea>
2018-11-27 09:08:08 +0100

    D

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ while choice !=3:
     try:
         choice = int(input("Välkommen till loggboken. \n 1. Läs Loggar \n 2. Skriv en logg. \n 3. Avsluta \n"))
     except:
-        print("Använd siffra! ")
+        print("Använd siffra!")
     if choice == 1:
         print("läs loggar")
     elif choice == 2: 
