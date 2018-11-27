4c5af9fc03e8079e98a134206aede56be16a7586
Pontus Erlingsson <erpo16@nti.umea>
2018-11-27 09:02:47 +0100

    message

---
+++ Main.py
@@ -0,0 +1,14 @@
+choice = 0
+message =""
+
+
+
+
+while choice !=3: 
+    choice = int(input("Välkommen till loggboken. \n 1. Läs Loggar \n 2. Skriv en logg. \n 3. Avsluta \n"))
+
+    if choice == 1:
+        print("läs loggar")
+    elif choice == 2: 
+        print("skriv en logg")
+    
