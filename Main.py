cc9445f718a907ba8302fa47139abfbcff3bd820
Pontus Erlingsson <erpo16@nti.umea>
2018-11-27 09:06:52 +0100

    try/except

--- Main.py
+++ Main.py
@@ -5,8 +5,11 @@ message =""
 
 
 while choice !=3: 
-    choice = int(input("Välkommen till loggboken. \n 1. Läs Loggar \n 2. Skriv en logg. \n 3. Avsluta \n"))
-
+    
+    try:
+        choice = int(input("Välkommen till loggboken. \n 1. Läs Loggar \n 2. Skriv en logg. \n 3. Avsluta \n"))
+    except:
+        print("Använd siffra! ")
     if choice == 1:
         print("läs loggar")
     elif choice == 2: 
