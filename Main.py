3bb588577af9e18e469f55cc085074deff5afd1a
Pontus Erlingsson <erpo16@nti.umea>
2018-11-27 09:37:02 +0100

    Stage 4

--- Main.py
+++ Main.py
@@ -10,6 +10,7 @@ while choice !=3:
         choice = int(input("Välkommen till loggboken. \n 1. Läs Loggar \n 2. Skriv en logg. \n 3. Avsluta \n"))
     except:
         print("Använd siffra!")
+        choice = 0
     if choice == 1:
         print("")
         f = open('log.txt', 'r')
@@ -17,6 +18,11 @@ while choice !=3:
             print(line)
         print("")
         f.close()
+        
     elif choice == 2: 
-        print("skriv en logg")
+        message = input("Skriv ett meddelande: ")
+        f = open('log.txt', 'a+')
+        f.write("\n" + message)
+        f.close()
+        
     
